4d7467b276e48742d43f26898a4acfbd2f3a9fc5
Yourlexs <you@example.com>
2021-10-28 13:53:13 +0300

    made edits in resending

--- controllers/user/resendingVerification.js
+++ controllers/user/resendingVerification.js
@@ -1,11 +1,11 @@
 const { resendingMail } = require('../../model/users')
 
 const resendingVerification = async (req, res) => {
-  const { mail } = req.body
-  if (!mail) {
+  const { email } = req.body
+  if (!email) {
     return res.status(400).json({ message: 'missing required fields' })
   }
-  await resendingMail(mail)
+  await resendingMail(email)
   res.status(200).json({ message: 'success' })
 }
 
